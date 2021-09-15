c312236301faefc3ec7739011c6ee91665ecb475
diuguide <everett.diuguid@gmail.com>
2021-09-15 15:56:33 -0400

    search all employees styled

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -3,7 +3,8 @@ import { Row, Col } from "react-bootstrap";
 const EmployeeCard = ({ emp }) => {
   console.log(emp);
   return (
-    <>
+    <Row className="d-flex justify-content-between">
+      <Col className="col-7">
         <Row>
           <Col>
             <div className="emp-name">
@@ -13,10 +14,16 @@ const EmployeeCard = ({ emp }) => {
         </Row>
         <Row>
           <Col>
-            <div className="emp-phone">{emp.phone_number != null ? emp.phone_number : "Not Available"}</div>
+            <div className="emp-phone">
+              {emp.phone_number != null ? emp.phone_number : "Not Available"}
+            </div>
           </Col>
         </Row>
-     </>
+      </Col>
+      <Col className="d-flex justify-content-end align-items-center">
+        <img src="./employee-logo.png" height="50" alt="employee"></img>
+      </Col>
+    </Row>
   );
 };
 
